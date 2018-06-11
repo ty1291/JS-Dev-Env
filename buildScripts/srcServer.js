e7d609b7cd41e9a712a5d636a4f5ec88b02b75b6
Tony Yu <tonyyu1991@gmail.com>
2018-06-11 19:57:11 +1000

    Add ESLint to solution

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,6 +6,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 const port = 3000;
 const app = express(); // instance of express
 const compiler = webpack(config);
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk'; // library that allows you to specify colour of output
 
-console.log(chalk.green('Start app in dev mode...'));
+console.log(chalk.green('Start app in dev mode...')); // eslint-disable-line no-console
