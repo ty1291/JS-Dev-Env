c07a81d9a4f634ac1a06dbb6ee451e7caaf4a3cb
Tony Yu <tonyyu1991@gmail.com>
2018-06-11 18:55:42 +1000

    Add transpiler Babel with slight code modification

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,11 +1,11 @@
 // Configure web server that will server up files in source directory
 
 var express = require('express');
-var path = require('path');
-var open = require('open');
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express(); // instance of express
+const port = 3000;
+const app = express(); // instance of express
 
 app.get('/', function(req, res) { // any references to root
   // __dirname: directory name we're currently running in
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk'); // library that allows you to specify colour of output
+import chalk from 'chalk'; // library that allows you to specify colour of output
 
 console.log(chalk.green('Start app in dev mode...'));
